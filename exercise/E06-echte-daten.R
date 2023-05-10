eef9bfe40c5be8ebd5be2ad9b11ecb608b15473a
Ludwigm6 <marvin.ludwig93@gmail.com>
2023-05-10 12:47:27 +0200

    session06

---
+++ exercise/E06-echte-daten.R
@@ -0,0 +1,16 @@
+# Nutze die Datei `crop_species.csv` aus dem Learnweb um folgende Fragen zu beantworten.
+
+
+## Wie viele Feldfrüchte wurden in der Studie von Meyer et al. 2019 untersucht?
+## Wie viele Plots gibt es pro Feldfrucht?
+## Auf welchem Plot wurden am meisten Individuen gesammelt?
+## Auf welchem Plot wurden die meisten Arten gefunden?
+
+## Wie viele Carabidae Individuen wurden im durchschnitt auf Plots mit Winterweizen gefunden?
+## Wie viele Individuen wurden Mimimal und Maximal für jeden Feldfruchttyp gefunden?
+## Visualisiere die Anzahl an Carabidae Individuen pro Feldfrucht.
+
+## Visualisiere die Anzahl der Araneae Arten in Relation zu der Anzahl der gesammelten Araneae Individuen.
+## Wenn auf einem Plot mehr Araneae Individuen gesammelt wurden, sind es dann auch mehr Araneae Arten?
+
+
