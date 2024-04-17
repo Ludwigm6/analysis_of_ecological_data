bb76b7931dbcba595f5e5f608af8c9895622e8d7
Ludwigm6 <marvin.ludwig93@gmail.com>
2024-04-17 15:20:45 +0200

    session2

---
+++ assignments/Ex01_besprechung.R
@@ -0,0 +1,14 @@
+# Berechne das Produkt deiner erstellten Variablen.
+x+y+z
+#Erbgebnis: 1795
+# Berechne den Mittelwert deiner erstellten Variablen.
+mean(x,y,z)
+#ergebnis: 1650
+#
+
+
+
+length(4, 3, 8, 90, 16)
+sum(4, 3, 8, 90, 16)
+sqrt(4, 3, 8, 90, 16)
+min(4, 3, 8, 90, 16)
